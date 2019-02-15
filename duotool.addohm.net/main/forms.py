5e46cdc3b7cfb43ee670641307008fe001987d42
Adam S. Leven <addohm@users.noreply.github.com>
2019-02-15 00:32:30 -0800

    Functioning test with success page and validation

--- duotool.addohm.net/main/forms.py
+++ duotool.addohm.net/main/forms.py
@@ -1,27 +1,77 @@
 from django import forms
+import os
+from django.conf import settings
+import json
 from pprint import pprint
 
 class TestForm(forms.Form):
     """
     Student test form
     """    
-    def __init__(self, testdict, *args, **kwargs):
+    def __init__(self, test_dict=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.testdict = {} if testdict is None else testdict
+        self._resource_path = os.path.join(settings.BASE_DIR, 'static/json')
+        self._json_path = os.path.join(self._resource_path, 'answers.json')
+        i = 0
+        if test_dict is not None:
+            self.test_dict = test_dict
+            # Save the test_dict
+            print('Saving the test dictionary')
+            with open(self._json_path, 'w') as f:
+                json.dump(test_dict, f, indent=2, ensure_ascii=False)
+            answer, wordid, question = self._unpack_dict(test_dict)
+        else:
+            # Open the test_dict
+            print('Reading the test dictionary')
+            with open(self._json_path, 'r', encoding='utf-8') as json_data:
+                test_dict = json.load(json_data)
+                json_data.close()
+            self.test_dict = test_dict
+            answer, wordid, question = self._unpack_dict(test_dict)
+        for i in range(len(question)):
+            field_name = 'testword' + wordid[i]
+            print(str(i))
+            print('id: ' + str(wordid[i]))
+            print('question: ' + question[i])
+            print('answer:' + answer[i])
+            self.fields[field_name] = forms.CharField(label=question[i], max_length=100)
+        self.question = question
+        self.wordid = wordid      
+        self.answer = answer
 
-    def dict_verify(self):
-        d = self.testdict
-        print('The dictionary supplied has a length of: ' + str(len(d)))
+    def clean(self):
+        print('CLEANING DATA')
+        context = {}
+        i = 0
+        # Open the test_dict
+        with open(self._json_path, 'r', encoding='utf-8') as json_data:
+            test_dict = json.load(json_data)
+            json_data.close()
+        answer, wordid, question = self._unpack_dict(test_dict)
+        # Get and check the results
+        for i in range(len(self.cleaned_data)):
+            field_name = 'testword' + wordid[i]
+            pprint(field_name)
+            result = self.cleaned_data.get(field_name)
+            pprint('Result is: ' + result)
+            pprint('Answers are: ' + str(answer))
+            if result != answer[i]:
+                self.add_error(field_name, 'Incorrect')
+            context[i] = question[i]
+            i += 1
+        return context
+
+    def _unpack_dict(self, test_dict):
+        answer = []
+        wordid = []
+        question = []
+        d = test_dict
         for word in d:
-            # print the base key
-            answer = word
+            answer.append(word)
             for key in d[word]:
                 value = str(d[word][key])
                 if key == 'id':
-                    field_name = value
+                    wordid.append(value)
                 if key == 'definition':
-                    question = value
-                # Print the 2nd level key and value pair
-                pprint(key + ': ' + str(d[word][key]))
-            self.fields[field_name] = forms.CharField(label=question, max_length=100)
-        
+                    question.append(value)
+        return answer, wordid, question
--- duotool.addohm.net/main/urls.py
+++ duotool.addohm.net/main/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
     # path('', views.home),
+    path('success/', views.success, name='success'),
     path('<str:username>/', views.home, name='home'),
-    path('<str:username>/test/', views.test, name='test'),
+    path('test/<str:username>/', views.test, name='test'),
 ]

d84620564f9dfcf34053c8c7b15ecdca747a778e
Adam S. Leven <addohm@users.noreply.github.com>
2019-02-15 00:31:28 -0800

    Added pinyin translation of characters, page nav, and audio on click

--- duotool.addohm.net/DuoTool/settings.py
+++ duotool.addohm.net/DuoTool/settings.py
@@ -15,12 +15,13 @@ from decouple import Csv, config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DEBUG', default=True, cast=bool)
+SECRET_KEY = config('KEY_HASH')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ duotool.addohm.net/main/views.py
@@ -0,0 +1,54 @@
+import requests
+import os
+import json
+from django.conf import settings
+from django.shortcuts import render
+
+
+def getUserData(url):
+    i = 0
+    char, wordlists = {}, {}
+    response = requests.get(url)
+    userdata = response.json()
+    # Import json for character statistics
+    json_path = os.path.join(settings.BASE_DIR, 'static/json/hanzidb.json')
+    # json_path = 'DuoTool/duotool.addohm.net/static/json/hanzidb.json'
+    with open(json_path, encoding='utf-8') as json_data:
+        word_data = json.load(json_data)
+        for lang in userdata['language_data']:
+            for item in userdata['language_data'][lang]['skills']:
+                i += 1
+                if item.get('levels_finished') > 0:
+                    explanation = {}
+                    explanation['index'] = i
+                    lessonwords = item.get("words")
+                    wordlists[str(lessonwords)] = []
+                    explanation['explanation'] = item.get("explanation")
+                    wordlists[str(lessonwords)].append(explanation)
+                    for word in lessonwords:
+                        pinyin = {}
+                        pinyin['index'] = i
+                        if word in word_data:
+                            pinyin['pinyin'] = word_data[word][0]['pinyin']
+                        else:
+                            pinyin['pinyin'] = ''
+                        char[word] = []
+                        char[word].append(pinyin)
+    return char, wordlists
+
+
+def home(request, username='addohm'):
+    template_name = 'main/index.html'
+
+    if request.method == 'POST':
+        username = request.POST['username']
+
+    url = "https://www.duolingo.com/users/{}".format(username)
+    char, wordlists = getUserData(url)  # catch value of dictionary
+    context = {
+        'username': username,
+        'char': char,
+        'wordlists': wordlists,
+    }
+    return render(request, template_name, context)
+
